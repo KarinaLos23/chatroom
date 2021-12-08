0220147536bfd1070ede7522d23cdd8627d2e4c9
Mar3kas <94892221+Mar3kas@users.noreply.github.com>
2021-12-08 13:03:30 +0200

    Add files via upload

---
+++ ApplicationUI/src/main/java/com/chatroomui/applicationui/ApplicationController.java
@@ -0,0 +1,37 @@
+package com.chatroomui.applicationui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ApplicationController {
+    @FXML private TextField textField;
+    @FXML private TextArea textArea;
+
+    private String username;
+    private String message;
+
+    @FXML
+    public void sendMessage() {
+        setMessage();
+        if (message.length() > 0 && message != null && !message.isEmpty()) {
+            textArea.appendText("\n" + username + ": "+ message);
+            textField.setText("");
+        }
+    }
+    protected void userJoinedChat(String username) {
+        textArea.appendText(username + " has joined the chat");
+    }
+
+    protected void setUsername(String username) {
+        this.username = username;
+    }
+
+    private void setMessage() {
+        this.message = textField.getText();
+    }
+
+    protected String getMessage() {
+        return message;
+    }
+}
---
+++ ApplicationUI/src/main/java/com/chatroomui/applicationui/LoginController.java
@@ -0,0 +1,46 @@
+package com.chatroomui.applicationui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class LoginController {
+    @FXML private TextField usernameField;
+    private String username;
+
+    @FXML
+    public void setNextScene(ActionEvent event) throws IOException {
+        URL url = new File ("C:\\Users\\Marijus\\Desktop\\Accenture\\chatroom\\ApplicationUI\\src\\main\\resources\\com\\chatroomui\\applicationui\\application.fxml").toURI().toURL();
+        FXMLLoader loader = new FXMLLoader(url);
+        Parent root = loader.load();
+
+        setUsername();
+
+        ApplicationController controller = loader.getController();
+        controller.setUsername(username);
+        controller.userJoinedChat(username);
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    private void setUsername() {
+        this.username = usernameField.getText();
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+}
+
